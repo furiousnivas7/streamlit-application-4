2de9022e313fd2b262107e27eaf02777f73c0d90
Nivash_sharma <nivasmathu@gmail.com>
2023-12-31 22:54:43 +0530

    import user input religion and photo

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import json
 from datetime import datetime
 import os
-import openai
-from openai import OpenAI
+# import openai
+# from openai import OpenAI
 
 def save_data_as_json(file_name):
     if os.path.exists(file_name):
@@ -11,9 +11,9 @@ def save_data_as_json(file_name):
             return json.dumps(json.load(file))
     return json.dumps([])
 
-def call_gbt3(prompt):
-    openai.api_key = os.environ['OPEN_API_KEY']
-    client=OpenAI()
+# def call_gbt3(prompt):
+#     openai.api_key = os.environ['OPEN_API_KEY']
+#     client=OpenAI()
 
     responce = client.completinons.create(
         model="gpt-3.5-turbo-instruct",  
@@ -55,10 +55,10 @@ def main():
     user_prompt = st.text_input("Enter your prompt for GPT-3.5")  
     button = st.button("Send Data to GPT-3.5") 
 
-    if button:
-        full_prompt = str(st.session_state.user_data_json) + user_prompt  
-        gpt3_response = call_gbt3(full_prompt)  
-        st.write(gpt3_response)  
+    # if button:
+    #     full_prompt = str(st.session_state.user_data_json) + user_prompt  
+    #     gpt3_response = call_gbt3(full_prompt)  
+    #     st.write(gpt3_response)  
 
 
     with st.form("user_info_form",clear_on_submit=True):
@@ -69,20 +69,28 @@ def main():
         work =st.text_input("work" ,placeholder="goverment or private")
         salary=st.number_input("salary",placeholder="enter your salary")
         dob = st.date_input("Date of Birth")
+        religion = st.text_input("Religion")
+        photo = st.file_uploader("Upload a photo")
 
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            user_data = {
-                "name": name,
-                "age":age,
-                "gender": gender,
-                "interest": interest,
-                "work":work,
-                "salary":salary,
-                "dob": dob.strftime("%Y-%m-%d")
-            }
-            save_data(user_data)
-            st.success("Data Saved Successfully!")
+
+        if name and age and gender and interest and work and salary and dob and religion and photo:
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                user_data = {
+                    "name": name,
+                    "age":age,
+                    "gender": gender,
+                    "interest": interest,
+                    "work":work,
+                    "salary":salary,
+                    "dob": dob.strftime("%Y-%m-%d"),
+                    "religion": religion,
+                    "photo": photo.read()
+                }
+                save_data(user_data)
+                st.success("Data Saved Successfully!")
+        else:
+            st.warning("Please fill in all required fields.")
 
     with open("user_data.json", "r") as json_file:
         st.download_button(
